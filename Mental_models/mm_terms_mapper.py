619010719f995689d738a009b4c814202362e2d5
jmsiqueiros <jmsiqueiros.bioetica@gmail.com>
2016-04-16 20:21:06 -0500

    Code has a command-line interface. Users are required to input a mental model edgelist csv file, a terms mapping json file, a name for the returning edgelist csv file and for the adjacency matrix csv file as well.

--- Mental_models/mm_terms_mapper.py
+++ Mental_models/mm_terms_mapper.py
@@ -1,20 +1,35 @@
 import networkx as nx
+import numpy as np
 import argparse
 import scipy as sp
+import pylab as pl
+import json
 
-#open the edgelist file of an original mental model
-edgelist = open(arg.edgelist)
-mapper = open(arg.mapper)
-output_edgelist = arg.outputedgelist
+parser = argparse.ArgumentParser(description='It changes the nodes labels of mental models with a list of standard labels for terms.')
+parser.add_argument('--dictionary', type=argparse.FileType('r'), required=True, help="Feed a json file with the mapping equivalences")
+parser.add_argument('--network', type=argparse.FileType('r'), required=True, help="Input a network.csv to which nodes names will be relabeled")
+parser.add_argument('--out_edgelist', type=str, required=True, help="Give me a name for your edgelist out_edgelist.csv file")
+parser.add_argument('--out_matrix', type=str, help="Give me a name for your adjacency matrix out_matrix.csv file")
+args    = parser.parse_args()
+
+
+#mapping = {'liquido':'agua','desecho':'basura'}
+mapping = json.load(args.dictionary) 
+
 
 #Creates a network from input edgelist
-g = nx.read_edgelist(edgelist, delimiter=',')
+g = nx.read_edgelist(args.network, delimiter=',')
+
 
 #Relabels in a new network
-h = nx.relabel_nodes(g, mapper)
+h = nx.relabel_nodes(g, mapping)
+
+nx.draw(h)
+pl.show()
 
 #Writes a file with the standardized terms network
-nx.write_edgelist(h, output_edgelist, delimiter=',')
+nx.write_edgelist(h, args.out_edgelist, delimiter=',')
 
 #Returns graph adjacency matrix
 a = nx.adjacency_matrix(h)
+np.savetxt(args.out_matrix, a, delimiter=",")
